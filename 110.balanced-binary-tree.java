d3973498a34c707c8fe82204147785667d2008a0
NathanDai <daizheng1998@qq.com>
2019-06-23 15:41:16 +0800

    2019 06 23 打卡

---
+++ 110.balanced-binary-tree.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return (Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1) 
+                  && isBalanced(root.left)
+                  && isBalanced(root.right);
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
